ed798307127fda5afd5a3bca2ca5a0ab610d04fe
randallrc <35576647+randallrc@users.noreply.github.com>
2019-07-11 17:16:48 -0400

    dark-mode

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleDarkLight() {
+    var body = document.getElementById("body");
+    var currentClass = body.className;
+    body.className = currentClass == "dark-mode" ? "light-mode" : "dark-mode";
+  }
+  
+  //add functionality to change emoji on button to sun
