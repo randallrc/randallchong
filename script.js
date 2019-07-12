63564daacb56f13f8814b4f8a700a3cf857e6d52
randallrc <35576647+randallrc@users.noreply.github.com>
2019-07-11 23:04:30 -0400

    change emoji

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function toggleDarkLight() {
     var body = document.getElementById("body");
     var currentClass = body.className;
+    //change css
     body.className = currentClass == "dark-mode" ? "light-mode" : "dark-mode";
+    //change emoji
+    body.className == "dark-mode" ?  document.getElementById("darklightbtn").innerHTML = "🌙" : document.getElementById("darklightbtn").innerHTML = "☀️";
   }
+  
